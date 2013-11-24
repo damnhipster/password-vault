acf9523285ba063f3ffbd3f6dc94d4d4bc5b1f77
laithnurie <laithnurie@gmail.com>
2013-11-24 00:55:45 +0000

    Update password_vault_spec.rb
    more tests written, tried to do it locally by the project was locked... sorry !

--- password_vault_spec.rb
+++ password_vault_spec.rb
@@ -1,16 +1,27 @@
 # bowling_spec.rb
 require_relative 'password_vault'
 
-describe PasswordVault, "#get_password" do
-  it "returns the password for given keys" do
-    password_vault = PasswordVault.new
-    password_vault.get_password_for("Amazon").should ==("amazon-password")
-    password_vault.get_password_for("Ebay").should ==("ebay-password")
+describe PasswordVault do
+  it "should return password when key is passed" do
+    @password_vault = PasswordVault.new
+    expect(@password_vault.retrieve_password("amazon")).to eql "amazon_password"
   end
 
-  it "returns the password for keys which are not case sensitive" do
-    password_vault = PasswordVault.new
-    password_vault.get_password_for("Amazon").should ==("amazon-password")
-    password_vault.get_password_for("amazon").should ==("amazon-password")
+  it "should return password when key in uppercase is passed" do
+    @password_vault = PasswordVault.new
+    expect(@password_vault.retrieve_password("Amazon")).to eql "amazon_password"
   end
+
+  it "should return password when key in uppercase is passed" do
+    @password_vault = PasswordVault.new
+    expect(@password_vault.retrieve_password(:Amazon)).to eql "amazon_password"
+  end
+
+  it "should add password when key and password is passed to it" do
+    @password_vault = PasswordVault.new
+    @password_vault.add_password("facebook","facebook_password")
+    expect(@password_vault.retrieve_password("facebook")).to eql "facebook_password"
+  end
+
+
 end
