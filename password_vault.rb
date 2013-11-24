61bf839ade6bb8a24de8d42e13ea849ce25afb83
Laith Nurie <laithnurie@gmail.com>
2013-11-24 01:02:47 +0000

    commenting locally

---
+++ password_vault.rb
@@ -0,0 +1,12 @@
+class PasswordVault
+   KEYS = {:amazon => "amazon_password", :ebay => "ebay_password"}
+
+   def retrieve_password(key)
+    key = key.downcase
+    KEYS[key.to_sym]
+   end
+
+  def add_password(symbol, password)
+    KEYS[symbol.to_sym] = password
+  end
+end
---
+++ password_vault_test.rb
@@ -0,0 +1,35 @@
+require 'rspec'
+
+require_relative "password_vault"
+
+describe PasswordVault do
+  it "should return password when key is passed" do
+    @password_vault = PasswordVault.new
+
+    expect(@password_vault.retrieve_password("amazon")).to eql "amazon_password"
+
+  end
+
+  it "should return password when key in uppercase is passed" do
+    @password_vault = PasswordVault.new
+
+    expect(@password_vault.retrieve_password("Amazon")).to eql "amazon_password"
+
+  end
+
+  it "should return password when key in uppercase is passed" do
+    @password_vault = PasswordVault.new
+
+    expect(@password_vault.retrieve_password(:Amazon)).to eql "amazon_password"
+
+  end
+
+  it "should add password when key and password is passed to it" do
+    @password_vault = PasswordVault.new
+    @password_vault.add_password("facebook","facebook_password")
+
+    expect(@password_vault.retrieve_password("facebook")).to eql "facebook_password"
+  end
+
+
+end
